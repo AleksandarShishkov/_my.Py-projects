dcbb0e2647117ef42b7b41c9edbc3ec012cae918
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-08 18:26:34 +0200

    Add files via upload

---
+++ Python_simple_program_2/Source.py
@@ -0,0 +1,51 @@
+
+#A simple Python program that calculates the tax to a certain amount set by the user
+
+
+tax_county = 0.02
+tax_state = 0.04
+
+
+def main():
+
+    message()
+
+    amount = float(input('\nWhat is the amount? > $'))
+
+    input("\nPress 'Enter' to calculate the taxes > ")
+
+    county_tax(amount)
+    state_tax(amount)
+    total_tax(amount)
+
+
+
+def county_tax(var):
+
+    tax = var * tax_county
+    print('\nThe county tax is: $',format(tax, '.2f'), sep='')
+
+
+def state_tax(var):
+
+    tax = var * tax_state
+    print('The state tax is: $', format(tax, '.2f'), sep='')
+
+
+
+def total_tax(var):
+
+    total = (var * tax_county) + (var * tax_state)
+    print('The total tax payed is: $', format(total, '.2f'), sep='')
+    print('\n')
+
+
+
+def message():
+    print('\n\tThis program will calculate the taxes for a certain amount inputed by the user.')
+    
+
+
+main()
+
+
