e2ef6f9fe3c5ccfc6e665d227af5992b5a7f04c5
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-20 14:40:47 +0200

    Update menu.py

--- Python_str_to_int/menu.py
+++ Python_str_to_int/menu.py
@@ -2,21 +2,21 @@
 
 
 
-def menu():                                             # definition of menu method
+def menu():                                                                     # definition of menu method
 
 
     print('\n\n\tSelect between the following:\n')
     print("'1' for string conversion to integers")
     print("'2' for integer conversion to string")
     print("'0' to quit the program")
-    choice = int(input('> '))                           # prompting the user to select the choice
+    choice = int(input('> '))                                                   # prompting the user to select the choice
 
-    while(choice < 0 or choice > 2):                    # validating the input
+    while(choice < 0 or choice > 2):                                            # validating the input
 
         choice = int(input('Try again> '))
 
 
-    return choice                                       # returning the choice
+    return choice                                                               # returning the choice
 
 
 
