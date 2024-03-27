a65d45e69c377f577a38e540eacef5d616778e70
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:35:49 +0200

    Update Question.py

--- Python_quiz_game/Question.py
+++ Python_quiz_game/Question.py
@@ -2,26 +2,25 @@
 
 
 
-class Question():                                                                                   # class Question
+class Question():                                                                                   
 
 
-    def __init__(self):                                                                             # default constructor
+    def __init__(self):                                                                             
 
-        self.__answer_D_pl1 = {}                                                                    # an empty disctinary for pl1`s answers
-        self.__quest_D_pl1 = {}                                                                     # an empty disctionary for pl1`s questions
-        self.__quest_D_pl2 = {}                                                                     # an empty dictionary for pl2`s questions
-        self.__answer_D_pl2 = {}                                                                    # an empty dictionary for pl2`s answers
-        self.__keys_pl1 = {}                                                                        # an empty dictionary for the pl1`s keys
-        self.__keys_pl2 = {}                                                                        # an empty dictionary for the pl2`s keys
-        self.__count = 1                                                                            # a counter set to 1
+        self.__answer_D_pl1 = {}                                                                    
+        self.__quest_D_pl1 = {}                                                                     
+        self.__quest_D_pl2 = {}                                                                     
+        self.__answer_D_pl2 = {}                                                                    
+        self.__keys_pl1 = {}                                                                        
+        self.__keys_pl2 = {}                                                                        
+        self.__count = 1                                                                            
         
         
 
 
 
-    def set_questions_pl1(self):                                                                    # setter for pl1`s questions                              
-                                                                                                    # setting the questions in the disctionary
-
+    def set_questions_pl1(self):                                                                                                  
+                                                                                                    
         self.__quest_D_pl1[1] = '\tHow many soccer players should each team have \n\ton the field at the start of each match?>'
         self.__quest_D_pl1[2] = '\tWhich Williams sister has won more \n\tGrand Slam titles?>'
         self.__quest_D_pl1[3] = '\tWhat country won the very first \n\tFIFA World Cup in 1930?>'
@@ -29,14 +28,14 @@ class Question():                                                               
         self.__quest_D_pl1[5] = '\tWhich planet is the hottest in the solar system?>'
 
 
-    def get_questions_pl1(self):                                                                    # getter for pl1`s uestions
+    def get_questions_pl1(self):                                                                    
 
-        return self.__quest_D_pl1                                                                   # returning the dictionary
+        return self.__quest_D_pl1                                                                   
 
 
 
-    def set_answers_pl1(self):                                                                      # setter for pl1`s answers
-                                                                                                    # setting the options in the dictionary
+    def set_answers_pl1(self):                                                                      
+                                                                                                    
 
         self.__answer_D_pl1[1] = '\n\t1. 10\n\t2. 11\n\t3. 13\n\t4. 15\n\t5. 20'
         self.__answer_D_pl1[2] = '\n\t1. Amy\n\t2. Serena\n\t3. Sam\n\t4. Vanesssa\n\t5. Kate'
@@ -46,14 +45,14 @@ class Question():                                                               
 
 
 
-    def get_answers_pl1(self):                                                                      # getter for pl1`s answers
+    def get_answers_pl1(self):                                                                      
 
-        return self.__answer_D_pl1                                                                  # returning the dictionary
+        return self.__answer_D_pl1                                                                  
 
 
 
-    def set_questions_pl2(self):                                                                    # setter for pl2`s questions
-                                                                                                    # setting the questions in the dictionary
+    def set_questions_pl2(self):                                                                    
+                                                                                                    
 
         self.__quest_D_pl2[1] = '\tWhat year was the very first model of the iPhone released?>'
         self.__quest_D_pl2[2] = "\tWhat`s the shortcut for the `copy` function on most computers?>"
@@ -62,14 +61,14 @@ class Question():                                                               
         self.__quest_D_pl2[5] = '\tWhich email service is owned by Microsoft?>'
 
 
-    def get_questions_pl2(self):                                                                    # getter for pl2`s questions
+    def get_questions_pl2(self):                                                                    
             
-        return self.__quest_D_pl2                                                                   # returning the dictionary
+        return self.__quest_D_pl2                                                                   
 
 
 
-    def set_answers_pl2(self):                                                                      # setter for pl2`s answers
-                                                                                                    # setting the options in the dictionary
+    def set_answers_pl2(self):                                                                      
+                                                                                                    
 
         self.__answer_D_pl2[1] = '\n\t1. 2019\n\t2. 2000\n\t3. 2005\n\t4. 2007\n\t5. None of the above'
         self.__answer_D_pl2[2] = '\n\t1. Ctr + C\n\t2. Shift + C\n\t3. Esc\n\t4. Ctr + K\n\t5. C'
@@ -78,14 +77,13 @@ class Question():                                                               
         self.__answer_D_pl2[5] = '\n\t1. GMail\n\t2. Hotmail\n\t3. Yahoo\n\t4. Outlook\n\t5. None of the above'
 
 
-    def get_answers_pl2(self):                                                                      # getter for pl2`s answers
+    def get_answers_pl2(self):                                                                      
 
-        return self.__answer_D_pl2                                                                  # returning the dictionary
+        return self.__answer_D_pl2                                                                  
 
 
-
-    def set_keys_pl1(self):                                                                         # setter for pl1`s correct answers
-                                                                                                    # setting the answers in the dictionary
+    def set_keys_pl1(self):                                                                         
+                                                                                                    
 
         self.__keys_pl1[1] = 2
         self.__keys_pl1[2] = 2
@@ -94,14 +92,14 @@ class Question():                                                               
         self.__keys_pl1[5] = 3
 
 
-    def get_keys_pl1(self, key):                                                                    # getter for pl1`s correct answers with parameter for the key
+    def get_keys_pl1(self, key):                                                                    
 
-        return self.__keys_pl1[key]                                                                 # returning the key
+        return self.__keys_pl1[key]                                                                 
 
 
 
-    def set_keys_pl2(self):                                                                         # setter for pl2`s correct answers
-                                                                                                    # setting the answers in the dictionary
+    def set_keys_pl2(self):                                                                         
+                                                                                                    
 
         self.__keys_pl2[1] = 4
         self.__keys_pl2[2] = 1
@@ -110,47 +108,46 @@ class Question():                                                               
         self.__keys_pl2[5] = 2
 
 
-    def get_keys_pl2(self, key):                                                                    # getter for pl2`s correct answers with parameter for the key
+    def get_keys_pl2(self, key):                                                                    
 
-        return self.__keys_pl2[key]                                                                 # returning the key
+        return self.__keys_pl2[key]                                                                 
 
 
 
    
 
 
-    def if_correct_answer_pl1(self, key):                                                           # setter for for the number of keys in the dictionaries for pl1
-                                                                                                    # with parameter for the current key
+    def if_correct_answer_pl1(self, key):                                                           
+                                                                                                    
 
-        del self.__answer_D_pl1[key]                                                                # deleting the record in __answer_D_pl1
-        del self.__quest_D_pl1[key]                                                                 # deleting the record in __quest_D_pl1
-        del self.__keys_pl1[key]                                                                    # deleting the record in __keys_pl1
+        del self.__answer_D_pl1[key]                                                                
+        del self.__quest_D_pl1[key]                                                                 
+        del self.__keys_pl1[key]                                                                    
         
 
 
 
-    def if_correct_answer_pl2(self, key):                                                           # setter for the number of keys in the dictionaries for pl2
-                                                                                                    # with parameter for the kye
+    def if_correct_answer_pl2(self, key):                                                           
+                                                                                                    
 
-        del self.__quest_D_pl2[key]                                                                 # deleting the record in __quest_D_pl2
-        del self.__answer_D_pl2[key]                                                                # deleting the record in __answer_d_pl2
-        del self.__keys_pl2[key]                                                                    # deleting the record in __keys_pl2
+        del self.__quest_D_pl2[key]                                                                 
+        del self.__answer_D_pl2[key]                                                                
+        del self.__keys_pl2[key]                                                                    
         
 
 
-    def null_count(self):                                                                           # re-setting the counter
+    def null_count(self):                                                                           
 
-        self.__count = 1                                                                            # setting __count to 1
-
-
-    def set_count(self):                                                                            # setting the counter
-
-        self.__count += 1                                                                           # adding 1 to __count
+        self.__count = 1                                                                            
 
 
-    def get_count(self):                                                                            # getter for the counter
+    def set_count(self):                                                                            
+
+        self.__count += 1                                                                           
+
+    def get_count(self):                                                                            
             
-        return self.__count                                                                         # returning the current counter
+        return self.__count                                                                         
 
 
 
