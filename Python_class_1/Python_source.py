dd427b3ee342ffef7f8f4e66b8d8e7d895f976ef
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:09:57 +0200

    Update Python_source.py

--- Python_class_1/Python_source.py
+++ Python_class_1/Python_source.py
@@ -2,64 +2,62 @@
 
 
 
-import Car                                                                                      # importing the Car module
+import Car                                                                                      
 
 
 def main():
 
 
-    user = 'y'                                                                                  # setting user to 'y'
+    user = 'y'                                                                                  
 
-    car = Car.Car(2010, 'Audi')                                                                 # an instance of the Car class
+    car = Car.Car(2010, 'Audi')                                                                 
 
-    car.get_speed()                                                                             # calling the get_speed() method
+    car.get_speed()                                                                             
+    while(user.lower() != '0'):                                                                 
 
-    while(user.lower() != '0'):                                                                 # a user controlled loop
+        user = input('\nIs the car accelerating (y/n or 0 to quit)> ')                          
 
-        user = input('\nIs the car accelerating (y/n or 0 to quit)> ')                          # promting the user to selec an option
+        if(user.lower() == 'y'):                                                                
+                acc = int(input('\nWith how much (km/h)> '))                                    
+                car.accelerate(acc)                                                             
 
-        if(user.lower() == 'y'):                                                                # validating the input
+                if(car.get_current_speed() > 400):                                              
 
-                acc = int(input('\nWith how much (km/h)> '))                                    # promting the user to enter a value
-                car.accelerate(acc)                                                             # calling the accelerate() method
-
-                if(car.get_current_speed() > 400):                                              # validating the current speed
-
-                    car.set_current_speed_acc()                                                 # calling the get_current_speed_acc() method
-                    car.get_speed()                                                             # printing the result
+                    car.set_current_speed_acc()                                                 
+                    car.get_speed()                                                             
                     print('\nThe car`s reached it`s maximum speed. Try to slow down!')
 
                 else:
 
-                    car.get_speed()                                                             # printing the result
+                    car.get_speed()                                                             
 
         elif(user.lower() == 'n'):
 
             
-            br = int(input('\nWith how much the car is slowing down (km/h)> '))                 # promting the user to enter a value
+            br = int(input('\nWith how much the car is slowing down (km/h)> '))                 
             car.brake(br)
             
-            if(car.get_current_speed() <= 0):                                                   # validating the input
+            if(car.get_current_speed() <= 0):                                                   
 
-                car.set_current_speed_br()                                                      # calling the set_current_speed_br() method
-                car.get_speed()                                                                 # printing the result
+                car.set_current_speed_br()                                                      
+                car.get_speed()                                                                 
                 print('The car`s stopped. No where to slow more!')
 
             else:
 
-                car.get_speed()                                                                 # printing the result
+                car.get_speed()                                                                 
 
         elif(user.lower() == 0):
 
-            break;                                                                              # breaking out of the loop
+            break;                                                                              
 
         elif(user.lower() != 'y' and user.lower() != 'n' and user.lower() != '0'):
 
-            print('\nInvalid input. Try again\n')                                               # printing a message if invalid input
+            print('\nInvalid input. Try again\n')                                               
 
 
 
-    print('\nThe program has ended!\n')                                                         # printing a message indicating the end of the program
+    print('\nThe program has ended!\n')                                                         
    
 
 main()
