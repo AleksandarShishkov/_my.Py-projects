9d4339ea185d7b9522f8c2665806b325cc70e486
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:04:10 +0200

    Update Profile_class.py

--- Python_Profile_class/Profile_class.py
+++ Python_Profile_class/Profile_class.py
@@ -2,17 +2,15 @@
 
 
 
-class Profile_class():                                                                          # Profile_class
-
+class Profile_class():                                                                          
     
-    __name = ' '                                                                                # a string to hold the name
-    __ID_num = ' '                                                                              # an integer to hold the ID
-    __department = ' '                                                                          # a string to hold the dept
-    __job_title = ' '                                                                           # a string to hold the job title
+    __name = ' '                                                                                
+    __ID_num = ' '                                                                              
+    __department = ' '                                                                          
+    __job_title = ' '                                                                           
 
 
-    def __init__(self):                                                                         # default constructor
-
+    def __init__(self):                                                                         
         self.__name = ' '
         self.__ID_num = ' '
         self.__department = ' '
@@ -20,45 +18,45 @@ class Profile_class():                                                          
 
 
 
-    def set_name(self):                                                                         # setter for the name
+    def set_name(self):                                                                         
 
         self.__name = input('Enter the name> ')
 
 
-    def set_ID(self):                                                                           # setter for the ID
+    def set_ID(self):                                                                           
 
         self.__ID_num = int(input('\nEnter the profile`s ID num> '))
 
 
 
-    def set_dept(self):                                                                         # setter for the dept
+    def set_dept(self):                                                                         
 
         self.__department = input('Enter the department> ')
 
 
 
-    def set_job_title(self):                                                                    # setter for the job title
+    def set_job_title(self):                                                                    
 
         self.__job_title = input('Enter the job title> ')
 
 
 
-    def get_name(self):                                                                         # getter for the name
+    def get_name(self):                                                                         
 
         return self.__name
 
 
-    def get_ID(self):                                                                           # getter for the ID
+    def get_ID(self):                                                                           
 
         return self.__ID_num
 
 
-    def get_dept(self):                                                                         # getter for the dept
+    def get_dept(self):                                                                         
 
         return self.__department
 
 
-    def get_job_title(self):                                                                    # getter for the job title
+    def get_job_title(self):                                                                    
 
         return self.__job_title
 
