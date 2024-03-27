afe3d3d9fa7c353eabe43eaf5d89b19f5bdc6565
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:22:54 +0200

    Update TotalScore.py

--- Python_guess_the_number/TotalScore.py
+++ Python_guess_the_number/TotalScore.py
@@ -1,12 +1,12 @@
 
 
 
-def totalScore(score):                                                              # defining totalScore() function which accepts an argument
-                                                                                    # validating the argument    
+def totalScore(score):                                                              
+                                                                                        
     if(score == 100):
         print('\n\n\n\t\tYou`ve reached 100 points.'\
             '\t\tGood for you . . .\n\n\n')
         
-        exit(0)                                                                     # exiting the program if the score reaches 100
+        exit(0)                                                                     
 
 
