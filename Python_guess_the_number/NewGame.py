775c8df2ff61ede09a32b2123f98da007e81ebdc
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:20:46 +0200

    Update NewGame.py

--- Python_guess_the_number/NewGame.py
+++ Python_guess_the_number/NewGame.py
@@ -1,10 +1,10 @@
 
 
-def new_game():                                                                     # defining new_game() function
+def new_game():                                                                     
 
-    new_game = str(input('\tContinue? (y/n)> '))                                    # prompting the user to input a letter (y/n)
+    new_game = str(input('\tContinue? (y/n)> '))                                    
 
-    while(new_game != 'y' and new_game != 'n'):                                     # validating the input
+    while(new_game != 'y' and new_game != 'n'):                                     
 
         new_game = input('\nInvalid input. Try agian '\
             '>')
@@ -14,9 +14,9 @@ def new_game():                                                                 
         print('\n\n\tYou are on the way of earning 100 points. . . \n')
         input('\tPress <Enter> to continue. . . \n')
 
-        return True                                                                 # returning True if the inputted letter is 'y'
+        return True                                                                 
     else:
         print('\n\n\tThe game has ended. . . Better luck next time!\n')
-        return False                                                                # returning False if the inputted letter is 'n'
+        return False                                                                
 
 
