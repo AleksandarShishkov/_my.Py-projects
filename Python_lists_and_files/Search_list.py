30467acccd8684670febe3ed55cd26de0aefe560
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:26:56 +0200

    Update Search_list.py

--- Python_lists_and_files/Search_list.py
+++ Python_lists_and_files/Search_list.py
@@ -4,13 +4,13 @@
 
 def serch(list_num, num):
 
-    if num in list_num:                                                             # serching for the inputted number
+    if num in list_num:                                                             
 
-        print("\n'", num, "' was located at index ", list_num.index(num))           # printing the location if on the list
+        print("\n'", num, "' was located at index ", list_num.index(num))           
 
     else:
 
-        print("\nThe number isn`t on the list")                                     # printing message if not on the list
+        print("\nThe number isn`t on the list")                                     
 
 
 
