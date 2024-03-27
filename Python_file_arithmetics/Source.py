b806a5bf2640e78960eb84d8893da2084b15ddfa
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:14:57 +0200

    Update Source.py

--- Python_file_arithmetics/Source.py
+++ Python_file_arithmetics/Source.py
@@ -1,50 +1,34 @@
 
 
-
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-        #  
-        #  This program prompts the user to create a .txt
-        #  file and to write series of integers to it. A menu
-        #  of options is then shown and asks the user to
-        #  select one of them. A result is calculated and shown.
-        #
-        #  The user exits the program once option '0' is 
-        #  selected from the menu.
-        #
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+import output_file                                              
+import input_file                                               
+import menu                                                     
+import option                                                   
 
 
+def main():                                                     
 
 
-import output_file                                              # importing the output_file module
-import input_file                                               # importing the input_file module
-import menu                                                     # importing the menu module
-import option                                                   # importing the option module
+    output_file.o_file()                                        
+    input_file.i_file()                                         
 
-
-def main():                                                     # declaring the main() function
-
-
-    output_file.o_file()                                        # calling the o_file() function
-    input_file.i_file()                                         # calling he i_file() function
-
-    choice = menu.menu_file()                                   # calling the menu_file() function
-                                                                # and assigning the returned
-                                                                # value to the choice variable
+    choice = menu.menu_file()                                   
+                                                                
+                                                                
     
-    while choice != '0':                                        # entering the program loop
+    while choice != '0':                                        
 
-        option.user_option(choice)                              # calling the user_option()
-                                                                # function, passing choice as
-                                                                # a parameter
+        option.user_option(choice)                              
+                                                                
+                                                                
 
-        choice = menu.menu_file()                               # calling the menu_file() function
-                                                                # and assigning the returned
-                                                                # value to the choice variable
+        choice = menu.menu_file()                               
+                                                                
+                                                                
     
 
-    print("The program has ended!\n\n")                         # printing message that the
-                                                                # program`s ended
+    print("The program has ended!\n\n")                         
+                                                                
 
   
 
@@ -52,6 +36,6 @@ def main():                                                     # declaring the 
 
 
 
-main()                                                          # calling the main() function
+main()                                                          
 
 
