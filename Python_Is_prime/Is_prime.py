e2a812df4244e3c2670dde91767a72f12f5f5709
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:02:20 +0200

    Update Is_prime.py

--- Python_Is_prime/Is_prime.py
+++ Python_Is_prime/Is_prime.py
@@ -2,30 +2,30 @@
 
 
 
-def prime(num):                                                                                     # defining the prime() method with an argument
-                                                                                                    # for the number
+def prime(num):                                                                                     
+                                                                                                    
 
-    if (num > 1):                                                                                   # validating the input
+    if (num > 1):                                                                                   
 
         for i in range(2, num):
 
             if(num % i == 0):
 
-                print("'", num, "' is not a prime number")                                          # printing a message indicating that the number isn`t
-                                                                                                    # prime since divisable not only by 1 and itself
+                print("'", num, "' is not a prime number")                                          
+                                                                                                    
 
-                break;                                                                              # breaking out of the loop
+                break;                                                                              
 
             else:
                
-                print("'", num, "' is a prime number")                                              # printing a message indicating that the number is
-                                                                                                    # prime
+                print("'", num, "' is a prime number")                                              
+                                                                                                    
 
-                break;                                                                              # breaking out of the loop
+                break;                                                                              
 
     else:
 
-        print("'", num, "' is not a prime number")                                                  # printing a message if the number is less than 1
+        print("'", num, "' is not a prime number")                                                  
 
 
 
