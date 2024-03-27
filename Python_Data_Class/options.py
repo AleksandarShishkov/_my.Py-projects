e9b46d0013de23e2fc0e6393a2ecfade2fea3f14
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:01:47 +0200

    Update options.py

--- Python_Data_Class/options.py
+++ Python_Data_Class/options.py
@@ -2,13 +2,13 @@
 
 
 
-def options():                                                      # options() method
-                                                                    # printing the options
+def options():                                                      
+                                                                    
     print('\n\tSelect between the following:\n')
     print('\t1 - for new profile')
     print('\t2 - to review the profile')
     print('\t0 - to quit')
 
-    choice = int(input('\n\t> '))                                   # promting the user to select an option
+    choice = int(input('\n\t> '))                                   
 
-    return choice                                                   # returning the choice
+    return choice                                                   
