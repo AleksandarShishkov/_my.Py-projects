3fcbc9b70789c52796c6f404067fb46d5b020db5
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:09:07 +0200

    Update Car_h.py

--- Python_class_1/Car_h.py
+++ Python_class_1/Car_h.py
@@ -2,51 +2,49 @@
 
 
 
-class Car():                                                                                    # class Car
+class Car():                                                                                    
    
-    __year = ' '                                                                                # a private integer to hold the year
-    __make = ' '                                                                                # a private string to hold the make
-    __speed = ' '                                                                               # a private integer to hold the speed
+    __year = ' '                                                                                
+    __make = ' '                                                                                
+    __speed = ' '                                                                               
 
 
 
 
-    def __init__(self, year, make):                                                             # constructor with parameters for the year          
-                                                                                                # and the make
+    def __init__(self, year, make):                                                                       
+                                                                                                
 
-        self.__year = year                                                                      # setting __year to year
-        self.__make = make                                                                      # setting __make to make
-        self.__speed = 0                                                                        # setting __speed to speed
-
-
-    def accelerate(self, acc):                                                                  # setter for the acceleration with an argument for
-                                                                                                # the value
-
-        self.__speed += acc                                                                     # adding value to __speed
+        self.__year = year                                                                      
+        self.__make = make                                                                      
+        self.__speed = 0                                                                        
 
 
-    def brake(self, br):                                                                        # setter for the break with an argument for
-                                                                                                # the value
+    def accelerate(self, acc):                                                                  
+                                                                                                
 
-        self.__speed -= br                                                                      # substracting the value to __speed
-
-
-    def get_current_speed(self):                                                                # getter for the current speed
-
-        return self.__speed                                                                     # returning __speed
+        self.__speed += acc                                                                     
 
 
-    def set_current_speed_acc(self):                                                            # setter for the acceleration maximum
+    def brake(self, br):                                                                        
+                                                                                                
 
-        self.__speed = 400                                                                      # assigning 400 to __speed
+        self.__speed -= br                                                                      
 
 
-    def set_current_speed_br(self):                                                             # setter for the breaking minimum
+    def get_current_speed(self):                                                                
+        return self.__speed                                                                     
 
-        self.__speed = 0                                                                        # assignin 0 to __speed
+
+    def set_current_speed_acc(self):                                                            
+        self.__speed = 400                                                                      
+
+
+    def set_current_speed_br(self):                                                             
+
+        self.__speed = 0                                                                        
         
 
-    def get_speed(self):                                                                        # getter for the result
+    def get_speed(self):                                                                        
 
         print('\nThe speed of the ', self.__make, ', year ', self.__year, ' is: ', self.__speed, 'km/h')
 
