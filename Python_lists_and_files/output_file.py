85d7e2e17da72f28aa24a7cd06e3368b923b0767
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:28:53 +0200

    Update output_file.py

--- Python_lists_and_files/output_file.py
+++ Python_lists_and_files/output_file.py
@@ -4,46 +4,45 @@
 
 def open_file():
 
-                                                                                    # prompting the user to enter the file name
+                                                                                    
     file_name = input("\nEnter a file name. Make sure " \
         "it ends with '.txt' extension> ")
 
-    output_file = open(file_name, 'w')                                              # crating the file
+    output_file = open(file_name, 'w')                                              
 
-                                                                                    # prompting the user to enter an integers
+                                                                                    
     print("\nEnter serie of integers between 1 and 10000." \
         "To quit press '-1'> ")
 
     num = int(input("> "))
 
-    while(num < -1 or num > 10000):                                                 # validating the input
+    while(num < -1 or num > 10000):                                                 
 
         print("\nInvalid input. Try again> ", end = ' ')
         num = int(input())
 
 
-    while(num != -1):                                                               # entering the sentinel-controlled loop
+    while(num != -1):                                                               
+        output_file.write(str(num) + '\n')                                          
 
-        output_file.write(str(num) + '\n')                                          # writing the data to the file
-
-                                                                                    # promting the user to enter the next integer
+                                                                                    
         print("\nEnter an integers between 1 and 10000." \
         "To quit press '-1'> ")
 
         num = int(input("> "))
 
-        while(num < -1 or num > 10000):                                             # validating the input
+        while(num < -1 or num > 10000):                                             
 
             print("\nInvalid input. Try again> ", end = ' ')
             num = int(input())
 
 
-    print("\nThe data`s been written to the file!\n")                               # printing a message indicating the data`s
-                                                                                    # been written to the file
+    print("\nThe data`s been written to the file!\n")                               
+                                                                                    
 
-    output_file.close()                                                             # closing the file
+    output_file.close()                                                             
 
 
 
-    return file_name                                                                # returning the file name
+    return file_name                                                                
 
