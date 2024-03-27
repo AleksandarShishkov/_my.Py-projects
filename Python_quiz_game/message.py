a15ccce0ca02d9dde6765f89c95c01d3d607316d
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:36:19 +0200

    Update message.py

--- Python_quiz_game/message.py
+++ Python_quiz_game/message.py
@@ -2,15 +2,15 @@
 
 
 
-def message():                                                                                      # message method
-                                                                                                    # printing the message in the beggining of the game
+def message():                                                                                      
+                                                                                                    
 
     print('\n\n\tThis is a trivia game. It is played by two players. ' +\
         '\n\tFirstly the players select their names, then they select who ' +\
         '\n\twill begin first. \n\n\tThere are 5 questions with 5 answers each. ' +\
         '\n\t1 point is gained per correct answer. \n\tThe first to gain 5 points wins the game!\n')
 
-    input('\n\n\tPress <Enter> key to continue. . .\n\n')                                           # promting the user to press <Enter> to continue
+    input('\n\n\tPress <Enter> key to continue. . .\n\n')                                           
 
 
 
