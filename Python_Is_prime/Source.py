c61bec5340abe556b1f12104befc52449a71e1dc
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:03:27 +0200

    Update Source.py

--- Python_Is_prime/Source.py
+++ Python_Is_prime/Source.py
@@ -1,20 +1,7 @@
 
 
-
-
-        # A Python program that prompts the user
-        # for an integer and calculates whether
-        # the inputted number is prime or not.
-        #
-        # The user can also select a range 
-        # trough which the numbers will be validated
-        # as prime or not
-
-
-
-
-import Menu                                                                                         # importing the Menu module
-import Is_prime                                                                                     # importing the Is_prime module
+import Menu                                                                                         
+import Is_prime                                                                                     
 
 
 
@@ -22,38 +9,38 @@ import Is_prime                                                                 
 def main():
 
 
-    choice = Menu.menu()                                                                            # an integer to hold the selected option
+    choice = Menu.menu()                                                                            
 
-    while(choice != 0):                                                                             # validating the option
+    while(choice != 0):                                                                             
 
         if(choice == 1):                                                            
 
-            num = int(input("\nEnter an integer> "))                                                # promting the user to enter an integer
-            Is_prime.prime(num)                                                                     # calling the prime() method
+            num = int(input("\nEnter an integer> "))                                                
+            Is_prime.prime(num)                                                                     
 
         elif(choice == 2):
 
-            print("\nEnter the range:")                                                             # promting the user to enter the starting and the ending
-            start = int(input("Start> "))                                                           # numbers for the range
+            print("\nEnter the range:")                                                             
+            start = int(input("Start> "))                                                           
             end = int(input("End> "))
 
             for i in range(start, end + 1, 1):
 
-                Is_prime.prime(i)                                                                   # calling the prime() method
+                Is_prime.prime(i)                                                                   
 
 
-        choice = Menu.menu()                                                                        # promting the user to select an option
+        choice = Menu.menu()                                                                        
 
 
-    print("\nThe program has ended\n")                                                              # printing a message indicating the end of the program if
-                                                                                                    # opriont '0' is selected
-
-
-
+    print("\nThe program has ended\n")                                                              
+                                                                                                    
 
 
 
-main()                                                                                              # calling main() function
+
+
+
+main()                                                                                              
 
 
 
