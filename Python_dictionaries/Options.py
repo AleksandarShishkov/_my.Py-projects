8d09189b99f1d525ee98036216cae0bbb473ac87
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:10:21 +0200

    Update Options.py

--- Python_dictionaries/Options.py
+++ Python_dictionaries/Options.py
@@ -2,13 +2,13 @@
 
 
 
-def menu(choice):                                                                                                   # defining menu() method with a parameter for
-                                                                                                                    # the choice
+def menu(choice):                                                                                                   
+                                                                                                                    
 
 
 
 
-    print("\n\t\tSelect between the following:\n")                                                                  # displaying the options
+    print("\n\t\tSelect between the following:\n")                                                                  
     print("\t'1' to create a contact with name and email")
     print("\t'2' to add a telephone number")
     print("\t'3' to add an address")
@@ -21,16 +21,16 @@ def menu(choice):                                                               
     print("\n\t'0' to exit")
 
 
-    choice = int(input("> "))                                                                                       # promting the user to enter the input
+    choice = int(input("> "))                                                                                       
 
-    while(choice < 0 or choice > 9):                                                                                # validating the input
+    while(choice < 0 or choice > 9):                                                                                
 
         print("\nInvalid input.")
         choice = int(intput("Try again> "))
 
 
 
-    return choice                                                                                                   # returning the input
+    return choice                                                                                                   
 
 
 
