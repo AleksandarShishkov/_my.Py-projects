4f88ce96eea1fefe58e3125d061232d979ace33e
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:21:04 +0200

    Update PC_num.py

--- Python_guess_the_number/PC_num.py
+++ Python_guess_the_number/PC_num.py
@@ -1,11 +1,10 @@
 
 
-import random                                                                       # iporting the random module
+import random                                                                       
 
 
-def pc_num():                                                                       # defining of pc_num() function
-
-    number = random.randrange(1, 100)                                               # assigning random number in number variable
-    print('\n\n\tGuess the number')                                                 # prinitng message
-    return number                                                                   # returning the number
+def pc_num():                                                                       
+    number = random.randrange(1, 100)                                               
+    print('\n\n\tGuess the number')                                                 
+    return number                                                                   
 
