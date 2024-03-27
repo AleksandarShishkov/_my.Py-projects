ceab53f12689a5c7d9360476b30c7f26a74be01e
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:23:06 +0200

    Update User.py

--- Python_guess_the_number/User.py
+++ Python_guess_the_number/User.py
@@ -1,10 +1,10 @@
 
 
 
-def user_input():                                                                   # defining user_input() function
+def user_input():                                                                   
 
-    guess = int(input('\nGuess> '))                                                 # assigning the users input to the variable guess
-    return guess                                                                    # returning the input
+    guess = int(input('\nGuess> '))                                                 
+    return guess                                                                    
 
 
 
