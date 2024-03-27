dc6366c076495c56f959535d8e680c967e49bbc9
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:15:46 +0200

    Update menu.py

--- Python_file_arithmetics/menu.py
+++ Python_file_arithmetics/menu.py
@@ -2,11 +2,11 @@
 
 
 
-def menu_file():                                                # decalring the menu_file()
-                                                                # function
+def menu_file():                                                
+                                                                
 
 
-                                                                # printing the options
+                                                                
     print("\n\n\tSelect between the following: " + \
         "\n'w' to re-write the file" +\
         "\n'r' to reviwe the file" +\
@@ -18,10 +18,10 @@ def menu_file():                                                # decalring the 
         "\n'avrg' to calculate the average of the items" +\
         "\n'0' to exit the program")
 
-    choice = input("> ")                                        # prompting the user to enter 
-                                                                # the choice
+    choice = input("> ")                                         
+                                                                
 
-                                                                # validating the input
+                                                                
     while(choice != 'w' and choice != 'r'
           and choice != 'a' and choice != "sum"
           and choice != "substr" and choice != "multiply"
@@ -31,4 +31,4 @@ def menu_file():                                                # decalring the 
         choice = input("Invalid input. Try again> ")
 
 
-    return choice                                               # returning choice
+    return choice                                               
