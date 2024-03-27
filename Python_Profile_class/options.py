cd9dea19beb5884ab8cbff3121b9e36f56270356
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 23:07:39 +0200

    Update options.py

--- Python_Profile_class/options.py
+++ Python_Profile_class/options.py
@@ -2,9 +2,9 @@
 
 
 
-def options():                                                                                  # options() method
+def options():                                                                                  
 
-    print('\n\tSelect between the following:\n')                                                # printing the options
+    print('\n\tSelect between the following:\n')                                                
     print('\t1 - look up an profile')
     print('\t2 - add new profile')
     print('\t3 - make changes to an existing profile')
@@ -13,10 +13,9 @@ def options():                                                                  
     print('\t6 - view the profiles')
     print('\t0 - quit the program')
 
-    choice = int(input('\n\t> '))                                                               # promting the user to select
-                                                                                                # an option
+    choice = int(input('\n\t> '))                                                               
+                                                                                                
 
-    return choice                                                                               # returning chocie
-
+    return choice                                                                               
 
 
