8150692e1471389409a915bc06c16d508f450c8f
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:51:05 +0200

    Update Source.py

--- Python_simple_program_5/Source.py
+++ Python_simple_program_5/Source.py
@@ -1,30 +1,29 @@
 
 
 
-def main():                                                                     # function main
+def main():                                                                     
 
-    rect1_width = float(input("\nEnter the width of the first rectangle > "))   # a float to hold width1
-    rect1_lenght = float(input("Enter the lenght of the first rectangle > "))   # a float to hold lenght1
+    rect1_width = float(input("\nEnter the width of the first rectangle > "))   
+    rect1_lenght = float(input("Enter the lenght of the first rectangle > "))   
 
-    rect2_width = float(input("Enter the width of the second rectangle > "))    # a float to hold width2
-    rect2_lenght = float(input("Enter the lenght of the second rectangle > "))  # a float to hold lenght2
+    rect2_width = float(input("Enter the width of the second rectangle > "))    
+    rect2_lenght = float(input("Enter the lenght of the second rectangle > "))  
 
-    compare_areas(rect1_width, rect1_lenght, rect2_width, rect2_lenght)         # calling compare_areas function
+    compare_areas(rect1_width, rect1_lenght, rect2_width, rect2_lenght)         
 
 
 
-def compare_areas(width1, lenght1, width2, lenght2):                            # compare_areas function which takes 4 float parameters
-                                                                                # which calculates and compares the areas of the two
-                                                                                # rectangles with relational if-else statement
+def compare_areas(width1, lenght1, width2, lenght2):                            
+                                                                                
+                                                                                
 
-    area1 = width1 * lenght1                                                    # calculating area1
-    area2 = width2 * lenght2                                                    # calculating area2
-
+    area1 = width1 * lenght1                                                    
+    area2 = width2 * lenght2                                                    
     if(area1 > area2):
         print("\nArea1 (", area1, ") is greater than area2 (", area2, ")\n")
     else:
         print("\nArea2 (", area2, ") is greater than area 1 (", area1, ")\n")
         
 
-main()                                                                          # calling main()
+main()                                                                          
 
