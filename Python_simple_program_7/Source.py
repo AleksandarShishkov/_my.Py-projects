51df83b2446904f75cdda14aad622e861af49eae
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:52:23 +0200

    Update Source.py

--- Python_simple_program_7/Source.py
+++ Python_simple_program_7/Source.py
@@ -1,29 +1,23 @@
 
 
-# A program that prompts the user to enter series of integers
-# and displaying their sum trought while loop after a negavie number is provided
+def main():                                                    
 
-
-def main():                                                    # definition of main()
-
-                                                               # taking an input from the user
+                                                               
     num = int(input("\nEnter and integer." + \
                     " To view the sum enter negative value > "))
 
-    total = 0                                                  # setting the total to '0'
+    total = 0                                                  
 
-    while (num > 0):                                           # entering the while loop
+    while (num > 0):                                           
 
-        total += num                                           # adding num to total
+        total += num                                           
 
-                                                               # taking an input from the user
+                                                               
         num = int(input("\nEnter and integer." + \
                     " To view the sum enter negative value > "))
 
 
-    print("\nThe total is : ", total, "\n")                    # printing the input
+    print("\nThe total is : ", total, "\n")                    
 
 
-main()                                                         # calling main
-
-
+main()                                                         
