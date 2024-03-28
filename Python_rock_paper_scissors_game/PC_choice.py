01369d2a29d9546cf7552bc861b793cf4b68db52
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:44:44 +0200

    Update PC_choice.py

--- Python_rock_paper_scissors_game/PC_choice.py
+++ Python_rock_paper_scissors_game/PC_choice.py
@@ -1,11 +1,10 @@
 
 
-import random                                                                       # impoting the random module
+import random                                                                       
 
 
-def pc_input():                                                                     # defining pc_input() function
+def pc_input():                                                                     
 
-    choice_pc = random.randrange(1, 4)                                              # assigning random number (1 - 3) to choice_pc
-    return choice_pc                                                                # returning choice_pc
-
+    choice_pc = random.randrange(1, 4)                                              
+    return choice_pc                                                                
 
