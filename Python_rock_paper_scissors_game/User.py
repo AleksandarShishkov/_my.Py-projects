a151cc5442b4b26933a410bf043010b18c78b78a
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:45:19 +0200

    Update User.py

--- Python_rock_paper_scissors_game/User.py
+++ Python_rock_paper_scissors_game/User.py
@@ -2,17 +2,17 @@
 
 
 
-def score_validation_pl(score_pl):                                                  # defining score_validation_pl() function
-                                                                                    # which accepts one argument for the pl`s score
-    if(score_pl == 10):                                                             # validating the score
+def score_validation_pl(score_pl):                                                  
+                                                                                    
+    if(score_pl == 10):                                                             
         print('\n\n\t\tYou have won the game.\n\n')
     else:
         print('\nYour score is: ', score_pl)
 
 
-def score_validation_pc(score_pc):                                                  # defining score_validation_pc() function
-                                                                                    # which accepts one argument for the pc`s score
-    if(score_pc == 10):                                                             # validating the score
+def score_validation_pc(score_pc):                                                  
+                                                                                    
+    if(score_pc == 10):                                                             
         print('\n\n\t\tThe PC has won the game.\n\n')
     else:
         print('PC`s score is: ', score_pc)
