81b2c5055361d3b5bf623536ea9d98b1488991d7
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:58:55 +0200

    Update num_digits.py

--- Python_string_menu/num_digits.py
+++ Python_string_menu/num_digits.py
@@ -2,13 +2,13 @@
 
 
 
-def digits(user):                                                                       # defining digits method with parameter for the string
+def digits(user):                                                                       
 
 
-    numInt = 0                                                                          # an integer to keep total of the integers located
-    total = 0                                                                           # an integer to keep the total
-    num_digits = []                                                                     # a list to hold the integers
-    digits = False                                                                      # a boolean to flag whether an intger is located
+    numInt = 0                                                                          
+    total = 0                                                                           
+    num_digits = []                                                                     
+    digits = False                                                                      
 
 
 
@@ -16,36 +16,28 @@ def digits(user):                                                               
 
         if(ord(user[i]) > 47 and ord(user[i]) < 58):                                    
                 
-            numInt += 1                                                                 # adding 1 to numInt if an integer is located
-            total += int(user[i])                                                       # converting the number to integer and adding it to total
-            num_digits += list(user[i])                                                 # adding the number to the num_digits list
-            digits = True                                                               # assigning True to digits
+            numInt += 1                                                                 
+            total += int(user[i])                                                       
+            num_digits += list(user[i])                                                 
+            digits = True                                                               
 
 
 
-    if(digits):                                                                         # valdiating digits
+    if(digits):                                                                         
 
-        print('\nString: ', user)                                                       # printing the string
-        print("Number of integers: ", numInt)                                           # printing the number of integers
-        print('Integers:            ', end = '')                                        # printing the numbers
+        print('\nString: ', user)                                                       
+        print("Number of integers: ", numInt)                                           
+        print('Integers:            ', end = '')                                        
 
         for i in num_digits:
 
             print(i, end = ' ')
 
 
-        print("\nTotal:              ", total)                                          # printing the total 
+        print("\nTotal:              ", total)                                           
 
 
     else:
 
-        print("\nNo integers located")                                                  # printing a message indicating that no integers
-                                                                                        # were located if none in the string
-
-
-
-
-
-
-
-
+        print("\nNo integers located")                                                  
+                                                                                        
