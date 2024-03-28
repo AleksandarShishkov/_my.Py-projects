e072d7a28976e3da90a10681eca2e42b001a19c0
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:59:25 +0200

    Update num_lower.py

--- Python_string_menu/num_lower.py
+++ Python_string_menu/num_lower.py
@@ -2,30 +2,26 @@
 
 
 
-def lower_case(user):                                                                   # defining ower_case method with parameter for the string
+def lower_case(user):                                                                   
 
 
-    lowerCase = 0                                                                       # an integer to keep count of the lower-case letters
-    lower_case = []                                                                     # a list to hold the lower-case letters
+    lowerCase = 0                                                                       
+    lower_case = []                                                                     
 
     for i in range(len(user)):
 
         if(user[i].islower()):
 
-            lowerCase += 1                                                              # adding 1 to lowerCase if a lower-case letter is located
-            lower_case += list(user[i])                                                 # adding the letter to the lower_case list
+            lowerCase += 1                                                              
+            lower_case += list(user[i])                                                 
 
 
 
 
-    print('\nString: ', user)                                                           # printing the stirng
-    print('Number of lower-case letters: ', lowerCase)                                  # printing the number of lower-case letters
-    print('Letters: ', end = '')                                                        # printing the letters
+    print('\nString: ', user)
+    print('Number of lower-case letters: ', lowerCase)                                  
+    print('Letters: ', end = '')                                                        
 
     for i in lower_case:
 
         print(i, end = ' ')
-
-
-
-
