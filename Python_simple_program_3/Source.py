af43b820f5cc266f9718963d74fbbf5821be2b35
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:50:01 +0200

    Update Source.py

--- Python_simple_program_3/Source.py
+++ Python_simple_program_3/Source.py
@@ -1,41 +1,36 @@
 
-
-#This program calculates the monthly and yearly costs for 
-#maintening an automobile
-
-
-def main():                                                                                 # main function definition
+def main():                                                                                 
 
     print('\n\tThis program will calculate the monthly and the yearly expenses \n\tfor an automobile\n')
 
-    loan = float(input('\nEnter the monthly loan expenses > $'))                            # a float holding the amount for the loan expences
-    insurance = float(input('Enter the monthly insurance expenses > $'))                    # a float holding the amount for the insurance
-    gas = float(input('Enter the monthly expenses for gas > $'))                            # a float holding the amount for the gas
-    oil = float(input('Enter the monthly expenses for the oil > $'))                        # a float holding the amount for the oil
-    tires = float(input('Enter the montly expenses for the tires > $'))                     # a float holding the amount for the tires
-    maintenance = float(input('Enter the monthly expenses for the maintenance > $'))        # a float holding the amount for the maintenance
+    loan = float(input('\nEnter the monthly loan expenses > $'))                            
+    insurance = float(input('Enter the monthly insurance expenses > $'))                    
+    gas = float(input('Enter the monthly expenses for gas > $'))                            
+    oil = float(input('Enter the monthly expenses for the oil > $'))                        
+    tires = float(input('Enter the montly expenses for the tires > $'))                     
+    maintenance = float(input('Enter the monthly expenses for the maintenance > $'))        
 
 
 
-    message()                                                                               # calling message()
-    total_monthly_expenses(loan, insurance, gas, oil, tires, maintenance)                   # calling total_monthly_expenses()
-    total_yearly_expenses(loan, insurance, gas, oil, tires, maintenance)                    # calling total_yearly_expenses()
+    message()                                                                               
+    total_monthly_expenses(loan, insurance, gas, oil, tires, maintenance)                   
+    total_yearly_expenses(loan, insurance, gas, oil, tires, maintenance)                    
 
 
 
 
-def message():                                                                              # a function to print the message
+def message():                                                                              
     print("\nPress 'Enter' to sum the totals : ")
 
-def total_monthly_expenses(_loan, _insurance, _gas, _oil, _tires, _maintenance):            # a function to sum the expenses 
+def total_monthly_expenses(_loan, _insurance, _gas, _oil, _tires, _maintenance):             
     
     total_m = _loan + _insurance + _gas + _oil + _tires + _maintenance
 
     print('\n\tThe monthly total is: $', format(total_m, ',.2f'), sep='')
 
 
-def total_yearly_expenses(_loan, _insurance, _gas, _oil, _tires, _maintenance):             # a function to to multiply the sum of the expenses by 12 - 
-                                                                                            # (for the months in 1 year of time)
+def total_yearly_expenses(_loan, _insurance, _gas, _oil, _tires, _maintenance):              
+                                                                                            
     total_y = (_loan + _insurance + _gas + _oil + _tires + _maintenance) * 12
 
     print('\tThe yearly total is: $', format(total_y, ',.2f'), sep='')
@@ -44,7 +39,5 @@ def total_yearly_expenses(_loan, _insurance, _gas, _oil, _tires, _maintenance): 
 
 
 
-main()                                                                                      # running the main()
-
-
+main()                                                                                      
 
