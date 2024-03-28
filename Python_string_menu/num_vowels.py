90b4495521d0ce733660b7f4bfe2ad0969f9fc8a
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 21:00:59 +0200

    Update num_vowels.py

--- Python_string_menu/num_vowels.py
+++ Python_string_menu/num_vowels.py
@@ -2,12 +2,12 @@
 
 
 
-def vowels(user):                                                                       # defining vowels method with parameter for
-                                                                                        # the string
+def vowels(user):                                                                       
+                                                                                        
 
-    numVowels = 0                                                                       # an integer to keep count of the number of vowels located
-    vowel_list = []                                                                     # a list to hold the vowels
-    vowel_located = False                                                               # a boolean to flag whether a vowel was located
+    numVowels = 0                                                                       
+    vowel_list = []                                                                     
+    vowel_located = False                                                               
 
 
     for i in range(len(user)):
@@ -16,19 +16,18 @@ def vowels(user):                                                               
            user[i].lower() == 'i' or user[i].lower() == 'u' or 
            user[i].lower() == 'o'):
 
-            numVowels += 1                                                              # addin 1 to numVowels if a vowel is located
-            vowel_list += list(user[i])                                                 # adding the vowel to the list
-            vowel_located = True                                                        # assigning true to vowel_located
-
+            numVowels += 1                                                              
+            vowel_list += list(user[i])                                                 
+            vowel_located = True                                                        
 
 
 
 
     if(vowel_located):
 
-        print('\nString: ', user)                                                       # printing the string
-        print('Number of vowels: ', numVowels)                                          # printing the number of vowels
-        print('Letters: ', end = '')                                                    # printing the vowels
+        print('\nString: ', user)                                                       
+        print('Number of vowels: ', numVowels)                                          
+        print('Letters: ', end = '')                                                    
    
         for i in vowel_list:
 
@@ -39,7 +38,5 @@ def vowels(user):                                                               
 
     else:
 
-        print('\nNo vowels were located!')                                              # printing a message indicating that no vowels
-                                                                                        # were located if none in the string
-
-
+        print('\nNo vowels were located!')                                              
+                                                                                        
