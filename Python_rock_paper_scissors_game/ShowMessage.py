0cf5b7eb460e0d68ec74977b39376d4e5814c4dd
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:45:02 +0200

    Update ShowMessage.py

--- Python_rock_paper_scissors_game/ShowMessage.py
+++ Python_rock_paper_scissors_game/ShowMessage.py
@@ -1,7 +1,7 @@
 
 
-def show_message():                                                                 # defining show_message() function
-                                                                                    # which prints the message
+def show_message():                                                                 
+                                                                                    
     print("\n\n\tThis is a game of 'Rock-Paper-Scissors'. In order "\
         "\n\tfor you to win the game, you have to put 10 score-points "\
         "\n\tin your account before the PC does.\n\n"\
@@ -9,6 +9,6 @@ def show_message():                                                             
         "\n\t'rock', 'paper' or 'scissors'.\n\n"\
         "\n\tGood luck!\n"\
         "\n\tPress <Enter> to continue . . . ")
-    input()                                                                         # promting the user to press <Enter> to continue
+    input()                                                                         
 
 
