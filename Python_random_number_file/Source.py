b23ad65e49ee81340ea50be2539a97cba9bddafc
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:43:01 +0200

    Update Source.py

--- Python_random_number_file/Source.py
+++ Python_random_number_file/Source.py
@@ -1,30 +1,7 @@
 
-
-
-
-        #
-        # This Python program creates a .txt file with
-        # user-defined name and writes to it randomly
-        # generated numbers which number and limit are
-        # also define by the user.
-        #
-        # The program displays the numbers, calculates their
-        # total and keep a running count for the number of
-        # values read.
-        #
-        # It also prompts the user to select whether additional
-        # numbers will be applied.
-        #
-
-
-
-
-
-
-
-import Output_r_file                                                                # importing Output_r_file
-import Input_r_file                                                                 # importing Input_r_file
-import Add_r_numbers                                                                # importing Add_r_numbers
+import Output_r_file                                                               
+import Input_r_file                                                                 
+import Add_r_numbers                                                                
 
 
 
@@ -34,26 +11,26 @@ import Add_r_numbers                                                            
 def main():
 
     
-    name_file = Output_r_file.o_file()                                              # calling o_file() function and assigning
-                                                                                    # the returned string to name_file
+    name_file = Output_r_file.o_file()                                              
+                                                                                    
 
-    Input_r_file.i_file(name_file)                                                  # calling i_file() function with parameter for
-                                                                                    # the file name
-
-
-    if(Add_r_numbers.a_file(name_file)):                                            # calling a_file() function with parameter for
-                                                                                    # the file name
-
-        Input_r_file.i_file(name_file)                                              # calling i_file() function with parameter for
-                                                                                    # the file name
+    Input_r_file.i_file(name_file)                                                  
+                                                                                    
 
 
+    if(Add_r_numbers.a_file(name_file)):                                            
+                                                                                    
 
-    print("\nThe program has ended\n")                                              # printing message indicating that the
-                                                                                    # program has ended
+        Input_r_file.i_file(name_file)                                              
+                                                                                    
 
 
 
+    print("\nThe program has ended\n")                                              
+                                                                                    
 
 
-main()                                                                              # calling the main() function
+
+
+
+main()                                                                              
