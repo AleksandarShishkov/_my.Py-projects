509b9fd8956f99dc6a238c237f877befd33045d9
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:51:45 +0200

    Update Source.py

--- Python_simple_program_6/Source.py
+++ Python_simple_program_6/Source.py
@@ -1,37 +1,29 @@
 
-# A program that makes some basic arithmetics calculations using a for loop
-# and an inputted value from the user. The program will display the amount of
-# dollars collected for 'n' amount of days as on the first day the user has 1 penny
-# which doubles every following day
+def main():                                                 
+
+    days = int(input("\nEnter the number of the days >"))   
+
+    calcAmount(days)                                        
 
 
-def main():                                                 # main function definition
+def calcAmount(days):                                       
 
-    days = int(input("\nEnter the number of the days >"))   # an int to hold the number of the days
+    total = 1;                                              
 
-    calcAmount(days)                                        # calling calcAmount() function
+    for x in range(1, days + 1):                            
 
-
-def calcAmount(days):                                       # calcAmount() definition
-
-    total = 1;                                              # setting the total to 1
-
-    for x in range(1, days + 1):                            # setting the for loop
-
-        total *= x                                          # calculating the total in the loop`s body
+        total *= x                                          
 
 
 
-    total /= 100                                            # deviding the total by 100 to get the 
-                                                            # amount in dollars
+    total /= 100                                             
+                                                            
 
 
-                                                            # printing the result
+                                                            
     print("\nThe total amount earned for ", days, \
           " days is $", format(total, ',.2f'), sep='')
 
 
 
-main()                                                      # calling main()
-
-
+main()                                                      
