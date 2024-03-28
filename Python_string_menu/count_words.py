a30df99a6339503157ac78579e885f649e2506a8
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:56:57 +0200

    Update count_words.py

--- Python_string_menu/count_words.py
+++ Python_string_menu/count_words.py
@@ -2,25 +2,22 @@
 
 
 
-def num_words(user):                                                                    # defining num_words method with parameter for
-                                                                                        # the string
+def num_words(user):                                                                    
+                                                                                        
 
 
-    countWords = 1                                                                      # an integer to keep count for the words
+    countWords = 1                                                                      
 
     for i in range(len(user)):
 
         if(user[i] == ' '):
-            countWords += 1                                                             # adding 1 to countWords if space is encountered
+            countWords += 1                                                             
 
         else:
             continue
 
     
-    print('\nString: ', user)                                                           # printing the string
-    print('Number of words: ', countWords)                                              # printing the number of words
+    print('\nString: ', user)                                                           
+    print('Number of words: ', countWords)                                              
 
    
-
-
-
