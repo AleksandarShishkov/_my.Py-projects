164c7b404d87c0e6c782ebdb4d32de2aab174138
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 20:55:57 +0200

    Update average_letters.py

--- Python_string_menu/average_letters.py
+++ Python_string_menu/average_letters.py
@@ -2,21 +2,21 @@
 
 
 
-def average(user):                                                                      # defining average method with parameter for the string
+def average(user):                                                                      
 
-    count = 1                                                                           # an integer to keep total of the words
+    count = 1                                                                           
 
     for i in range(len(user)):
 
         if(user[i] == ' '):
 
-            count += 1                                                                  # adding 1 to count if an empty space is encountered
+            count += 1                                                                  
 
 
-    average = len(user) / count                                                         # calculating the average letters
+    average = len(user) / count                                                         
 
 
-    print('\nString: ', user)                                                           # printing the string
-    print('Average letters in a word: ', format(average, '.2f'))                        # printing the formatted average
+    print('\nString: ', user)                                                           
+    print('Average letters in a word: ', format(average, '.2f'))                        
 
 
